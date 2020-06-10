05fc906cac5c8e42a85524bcd0234ffc5869d0cb
mtsluna <mtsluna@gmail.com>
2020-06-10 10:11:06 -0300

    Pagination reformed, add new header to use the pagination

---
+++ src/Event/AddPaginationHeaders.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Event;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Paginator;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class AddPaginationHeaders implements EventSubscriberInterface
+{
+    public function addHeaders(ResponseEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (($data = $request->attributes->get('data')) && $data instanceof Paginator) {
+            $from = $data->count() ? ($data->getCurrentPage() - 1) * $data->getItemsPerPage() : 0;
+            $to = $data->getCurrentPage() < $data->getLastPage() ? $data->getCurrentPage() * $data->getItemsPerPage() : $data->getTotalItems();
+            $lastPage = $data->getLastPage();
+            $currentPage = $data->getCurrentPage();
+
+            $response = $event->getResponse();
+            $response->headers->add([
+                'Content-Range' => \sprintf('%u-%u/%u', $from, $to, $data->getTotalItems()),
+                'Page-Range' => \sprintf('%u/%u', $currentPage, $lastPage)
+            ]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => 'addHeaders',
+        ];
+    }
+}
